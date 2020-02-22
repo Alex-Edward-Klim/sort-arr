c4d7a660e19e748b7c7e459ae2efcb9ae911b3bf
Alex-Edward-Klim <alexklim3221128@gmail.com>
2020-02-22 21:19:12 +0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Folders';
+
+  arr = [2, 4, 1, 3, 5];
+
+  sortArr() {
+    const foo = (a, b) => a - b;
+    this.arr.sort(foo);
+  }
 }
